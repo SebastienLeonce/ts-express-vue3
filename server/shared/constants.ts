6138f9a47d6dcefc4ecc42c12acf5c9810f0d5c7
Sébastien LEONCE <sebastienleonce91@gmail.com>
2021-11-23 16:27:51 +0100

    update cookie

--- server/shared/constants.ts
+++ server/shared/constants.ts
@@ -15,8 +15,7 @@ export const cookieProps = Object.freeze({
         signed: true,
         path: (process.env.COOKIE_PATH),
         maxAge: Number(process.env.COOKIE_EXP),
-        domain: (process.env.COOKIE_DOMAIN),
-        secure: (process.env.SECURE_COOKIE === 'true'),
+        secure: process.env.NODE_ENV === "production",
         sameSite: false
     },
 });
